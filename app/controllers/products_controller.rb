15c609650b941a606ea4e65f234b1f8a080295b5
Sean McNally <seanmcnally88@gmail.com>
2015-11-15 17:03:29 -0600

    Added update functionality

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -22,6 +22,23 @@ class ProductsController < ApplicationController
       description: params[:description],
       deliciousness: params[:deliciousness]
       )
+    redirect_to '/products'
+  end
+
+  def edit
+    product_id = params[:id]
+    @product = Product.find_by(id: params[:id])
+  end
+
+  def update
+    @product = Product.find_by(id: params[:id])
+    @product.update(
+      name: params[:name],
+      price: params[:price],
+      description: params[:description],
+      deliciousness: params[:deliciousness]
+      )
+    redirect_to "/products/#{@product.id}"
   end
 
   def checkout
