13c35ea9a73c19ef50c6cac9796d4f32e061aed0
Sean McNally <seanmcnally88@gmail.com>
2015-11-29 10:28:55 -0600

    Changed integer attribtues to readable Heroku attributes

--- db/migrate/20151122215424_add_id_to_supplier.rb
+++ db/migrate/20151122215424_add_id_to_supplier.rb
@@ -1,5 +1,5 @@
 class AddIdToSupplier < ActiveRecord::Migration
   def change
-    add_column :suppliers, :supplier_id, :integer
+    add_column :suppliers, :supplier_id 'integer USING CAST(supplier_id AS integer)'
   end
 end
--- db/migrate/20151122215804_add_id_to_product.rb
+++ db/migrate/20151122215804_add_id_to_product.rb
@@ -1,5 +1,5 @@
 class AddIdToProduct < ActiveRecord::Migration
   def change
-    add_column :products, :supplier_id, :integer
+    add_column :products, :supplier_id, 'integer USING CAST(supplier_id AS integer)'
   end
 end
