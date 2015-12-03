5da0eea1cc8c766568e83be567b7facc32f9de12
Sean McNally <seanmcnally88@gmail.com>
2015-12-02 21:04:30 -0600

    Cleaned up orders controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -11,7 +11,6 @@ class OrdersController < ApplicationController
     @subtotal = 0
     @tax = 0
     @carted_products.each do |carted_product|
-      carted_product.status = "purchased"
       product_id = carted_product.product.id
       price = carted_product.product.price
       quantity = carted_product.quantity
